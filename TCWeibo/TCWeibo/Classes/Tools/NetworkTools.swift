a5ad4d50191a928c848256cfa6fad46c4a510386
TailC <wlxlj24@qq.com>
2016-05-30 16:39:38 +0800

    修改单例类

--- TCWeibo/TCWeibo/Classes/Tools/NetworkTools.swift
+++ TCWeibo/TCWeibo/Classes/Tools/NetworkTools.swift
@@ -11,21 +11,10 @@ import AFNetworking
 
 class NetworkTools: AFHTTPSessionManager {
 	
-	static var instance: NetworkTools?
-	static var token: dispatch_once_t = 0
+	private static let instance = NetworkTools()
 	
-	class func shareNetworkTools() -> NetworkTools {
-		dispatch_once(&token) { () -> Void in
-			self.instance = NetworkTools()
-			
-			let baseURL = NSURL(string: "https://api.weibo.com/")
-			let tools = NetworkTools(baseURL:baseURL)
-			
-			// 设置反序列化数据格式集合
-			tools.responseSerializer.acceptableContentTypes = NSSet(objects: "application/json", "text/json", "text/javascript", "text/plain") as? Set<String>
-			
-		}
-		return instance!
+	class var sharedInstance: NetworkTools {
+		return instance
 	}
 	
 }
